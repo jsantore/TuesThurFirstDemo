015c91290dd772346469883d5d76bf7993eed59e
shinjitsu <shinjitsu@auxilioabalto.me>
2025-09-10 13:12:31 -0400

    reporting on students here

--- shoutExample.py
+++ shoutExample.py
@@ -5,3 +5,6 @@ num_credits = input("How many credits is that class:")
 print(f"You are taking {class_name} next semester for {num_credits} credits")
 STUDENTS_IN_CLASS = 23
 students_today = 21
+students_absent = STUDENTS_IN_CLASS - students_today
+percent_here = students_today/STUDENTS_IN_CLASS*100
+print(f"There are {STUDENTS_IN_CLASS} registered and {percent_here:.2f}% of there are here today")
